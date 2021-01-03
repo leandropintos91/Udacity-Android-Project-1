25afc16b4ff300638ba0bc3edcf4ac80e1fc2ac9
Leandro Pintos <leopintos11@gmail.com>
2021-01-03 02:02:38 -0300

    Create Welcome Screen

---
+++ starter/app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
@@ -0,0 +1,25 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+
+    lateinit var binding : FragmentWelcomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
+        return binding.root
+    }
+
+}
