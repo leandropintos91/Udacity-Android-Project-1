905da18f2de6545f8b89c23977f8ddcdcd68a68f
Leandro Pintos <leopintos11@gmail.com>
2021-01-03 03:43:09 -0300

    Create Shoes List Screen and navigation to it

--- starter/app/src/main/java/com/udacity/shoestore/InstructionsFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/InstructionsFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.udacity.shoestore.databinding.FragmentInstructionsBinding
 
 class InstructionsFragment :Fragment() {
@@ -19,6 +20,13 @@ class InstructionsFragment :Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_instructions, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.instructionsGoToShoesList.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_instructionsFragment_to_shoesListFragment)
+        )
     }
 
 }
