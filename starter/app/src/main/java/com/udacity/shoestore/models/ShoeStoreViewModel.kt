47a796f252dd6022d178226368f6afd91c53fb3f
Leandro Pintos <leopintos11@gmail.com>
2021-01-03 18:48:32 -0300

    Use data on details layout

--- starter/app/src/main/java/com/udacity/shoestore/models/ShoeStoreViewModel.kt
+++ starter/app/src/main/java/com/udacity/shoestore/models/ShoeStoreViewModel.kt
@@ -4,17 +4,19 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ShoeStoreViewModel : ViewModel() {
+
+    val shoesList : MutableLiveData<MutableList<Shoe>> = MutableLiveData()
+    val shoe = MutableLiveData<Shoe>()
+
+    init {
+        shoesList.value = mutableListOf()
+    }
+
     fun addNewShoe(pName: String, pCompany: String, pSize: String, pDescription: String) {
         shoesList.value!!.add(Shoe(name = pName, company = pCompany, size = pSize.toDouble(), description = pDescription))
     }
 
     fun clearData() {
         shoesList.value!!.clear()
-    }
-
-    val shoesList : MutableLiveData<MutableList<Shoe>> = MutableLiveData()
-
-    init {
-        shoesList.value = mutableListOf()
     }
 }
